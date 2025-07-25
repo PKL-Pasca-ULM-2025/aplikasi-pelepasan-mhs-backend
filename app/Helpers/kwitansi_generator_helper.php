a352955e4b1b2c084b8f7740e6b8905ac1ab6943
ozannaufal15 <ozan.naufal15@gmail.com>
2025-07-25 13:49:35 +0800

    Tambah fungsi generate_kuitansi dan dependensi dompdf

---
+++ app/Helpers/kwitansi_generator_helper.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Summary of generate_kwitansi
+ * @return string
+ */
+function generate_kwitansi(){
+    // Function to generate a kwitansi (receipt)
+    // This is a placeholder function and should be implemented with actual logic
+    return "Kwitansi generated successfully.";
+}
