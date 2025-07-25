3f7d7fc7d079f8814c1fe5fe501af49665a7dd4c
ozannaufal15 <ozan.naufal15@gmail.com>
2025-07-25 13:52:33 +0800

    Edit typo

--- app/Helpers/kwitansi_generator_helper.php
+++ app/Helpers/kwitansi_generator_helper.php
@@ -4,7 +4,7 @@
  * Summary of generate_kwitansi
  * @return string
  */
-function generate_kwitansi(){
+function generate_kuitansi(){
     // Function to generate a kwitansi (receipt)
     // This is a placeholder function and should be implemented with actual logic
     return "Kwitansi generated successfully.";
